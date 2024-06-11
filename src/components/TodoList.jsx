188da8dfd370a8a32a40e38cb8b44e843055eb6f
Rahul Shah <rahulshah0728@gmail.com>
2024-06-12 01:15:16 +0530

    removed comment unncessary comment and added comment in todoSlice.js

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux'; 
 import TodoItem from './TodoItem';
-// import { fetchTodos } from '../redux/todoSlice';
 import { fetchTodosData } from '../todos/todoThunks';
 
 const TodoList = () => {
--- src/todos/todoSlice.js
+++ src/todos/todoSlice.js
@@ -14,25 +14,31 @@ const todosSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
+    //Adding case for during we fetch the data and loading is true - fetchPart
       .addCase(fetchTodosData.pending, (state) => {
         state.loading = true;
       })
-      .addCase(fetchTodosData.fulfilled, (state, action) => {
-        state.loading = false;
-        state.todos = action.payload;
-      })
+    //Adding case for which data fetch perfectly and do loading false  - fetchPart
+    .addCase(fetchTodosData.fulfilled, (state, action) => {
+      state.loading = false;
+      state.todos = action.payload;
+    })
+    //Adding case for when data not loaded perfectly or have any error and do loading false  - fetchPart
       .addCase(fetchTodosData.rejected, (state, action) => {
         state.loading = false;
         state.error = action.error.message;
       })
+    // Adding case for todo in list 
       .addCase(addTodoData.fulfilled, (state, action) => {
         state.todos.push(action.payload);
       })
+    // Adding case for completeTodo
       .addCase(completeTodoData.fulfilled, (state, action) => {
         const index = state.todos
         .findIndex((todo) => todo._id === action.payload._id);
         state.todos[index] = action.payload;
       })
+    // Adding case for Deleting todo from list
       .addCase(deleteTodoData.fulfilled, (state, action) => {
         state.todos = state.todos
         .filter((todo) => todo._id !== action.payload);
